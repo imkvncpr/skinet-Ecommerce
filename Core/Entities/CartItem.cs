dbf0acb12af9363a2fcf7bbd9bfb7a4533a66769
Kevon Cooper <imkvncpr@gmail.com>
2024-10-22 16:14:18 -0400

    Api||ShoppingCart.RedisDatabase

--- Core/Entities/CartItem.cs
+++ Core/Entities/CartItem.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Core.Entities;
 
 public class CartItem
@@ -7,8 +5,8 @@ public class CartItem
     public int ProductId { get; set; }
     public required string ProductName { get; set; }
     public decimal Price { get; set; }
-    public required int Qunatity { get; set; }
+    public required int Quantity { get; set; }  // Fixed typo from "Qunatity" to "Quantity"
     public required string PictureUrl { get; set; }
     public required string Brand { get; set; }
     public required string Type { get; set; }
-}   
+}
